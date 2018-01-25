a50c233ebcc1451ab72652e42fb8595cc41d6cac
H.Xu <hxm14375@outlook.com>
2018-01-25 17:21:08 +0800

    Create 整理2.ps1

---
+++ 整理2.ps1
@@ -0,0 +1,27 @@
+#丢av号文件夹下面运行 
+#by Misaka14375
+$currentPath=Split-Path -Parent $MyInvocation.MyCommand.Definition #当前文件夹
+$foo =($currentPath).Split('\\') 
+$av=$foo[$foo.Count-1] #av号
+Get-ChildItem -Filter *.blv -Recurse |
+ForEach-Object{
+    $bar=($_.DirectoryName).Split('\\')
+    $partNumber=$bar[$bar.Count-2] #分p号
+    cd $_.DirectoryName
+    $newName = 'av{0}-p{1}-{2}' -f $av,$partNumber,$_.Name.Replace(".blv",".flv")
+    Rename-Item -Path $_.FullName -NewName $newName
+    Move-Item $newName -Destination ../../
+}
+cd $currentPath
+
+Get-ChildItem -Filter *.xml -Recurse |
+ForEach-Object{
+    $bar=($_.DirectoryName).Split('\\')
+    $partNumber=$bar[$bar.Count-1] #分p号
+    cd $_.DirectoryName
+    $newName = 'av{0}-p{1}.xml' -f $av,$partNumber
+    Rename-Item -Path $_.FullName -NewName $newName
+    Move-Item $newName -Destination ../
+}
+cd $currentPath
+Remove-Item ./* -Exclude *.flv,*.xml,*.ps1 -Recurse #删掉其他文件
