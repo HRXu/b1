be04fffa3ac508386a59f6830216153d6eef4269
H.Xu <hxm14375@outlook.com>
2018-08-12 17:49:42 +0800

    Update b站缓存整理2.ps1

--- b站缓存整理2.ps1
+++ b站缓存整理2.ps1
@@ -1,32 +1,39 @@
 #丢av号文件夹下面运行 
-#by Misaka14375
+#需要安装ffmpeg 添加环境变量
+#按p整理，安装版滋瓷合并分段
+#用前备份
 $type = Read-Host "Choose 0 or 1 //Android[0] UWP[1]"
 $currentPath=Split-Path -Parent $MyInvocation.MyCommand.Definition #当前文件夹
 if($type -eq "0")
 {
-    $foo =($currentPath).Split('\\') 
-    $av=$foo[$foo.Count-1] #av号
     Get-ChildItem -Filter *.blv -Recurse |
-    ForEach-Object{
-        $bar=($_.DirectoryName).Split('\\')
-        $partNumber=$bar[$bar.Count-2] #分p号
+    ForEach-Object{       
         cd $_.DirectoryName
-        $newName = 'av{0}-p{1}-{2}' -f $av,$partNumber,$_.Name.Replace(".blv",".flv")
-        Rename-Item -Path $_.FullName -NewName $newName
-        Move-Item $newName -Destination ../
+        Move-Item $_.Name -Destination ../    
+        Out-File -FilePath ../filelist.txt -Append -Encoding ASCII -InputObject "file $_" 
     }
     cd $currentPath
+#弹幕
     Get-ChildItem -Filter *.xml -Recurse |
     ForEach-Object{
         $bar=($_.DirectoryName).Split('\\')
         $partNumber=$bar[$bar.Count-1] #分p号
         cd $_.DirectoryName
         $newName = 'av{0}-p{1}.xml' -f $av,$partNumber
-        Rename-Item -Path $_.FullName -NewName $newName
-        Move-Item $newName -Destination ../
+        Rename-Item -Path $_.FullName -NewName $newName    
     }
     cd $currentPath
-    Remove-Item ./* -Exclude *.flv,*.xml,*.ps1 -Recurse #删掉其他文件
+
+    Get-ChildItem -Filter *.txt -Recurse |
+    ForEach-Object{
+        $bar=($_.DirectoryName).Split('\\')
+        $partNumber=$bar[$bar.Count-2] #分p号
+        $file='av{0}-p{1}.mkv' -f $av,$partNumber
+        cd $_.DirectoryName
+        ffmpeg -f concat -i filelist.txt -c copy "$file"
+    }
+    cd $currentPath
+    Remove-Item ./* -Exclude *.mkv,*.xml,*ps1 -Recurse #删掉其他文件
 }
 else
 {
